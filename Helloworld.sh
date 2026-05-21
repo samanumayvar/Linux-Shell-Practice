a3bbca78cb973aa5522939a834a091243e2bfe7e
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-21 18:18:39 +0530

    new one

---
+++ Helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello World!"
+echo "Welcome to Linux Shell Scripting Practice!"
+sleep 10
+echo "Goodbye!"
+echo "See you next time!"
+sleep 5
+echo "Exiting the script now."  
