3af993ef63db4696ce56b7c42a1c9886ee1b9ad5
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-23 16:00:14 +0530

    new variables

--- Variables.sh
+++ Variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 Person1=$1  
 Person2=$2
-BASH=$
+BASH=$3
 echo"$Person1: $Person2 i will $BASH you to the stone age"
 echo "$Person2: We will $BASH you to the stone age too"
 echo "$Person1: I will $BASH you to the stone age again"
 echo "$Person2: We will $BASH you to the stone age again too"
 echo "$Person1: I will $BASH you to the stone age again and again"
 echo"$Person2: we will stop harmozgan and $BASH you to the stone age again and again"
-echo "$Person1: I will $BASH you to the stone age again and again and again"
+echo "$Person1: I will $BASH you to the stone age again and again and again"gi
