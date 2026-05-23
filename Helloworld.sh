949d3df7d48c2a41f9caccebf1eaa217d4375461
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-23 15:20:23 +0530

    third commit

--- Helloworld.sh
+++ Helloworld.sh
@@ -2,10 +2,13 @@
 
 echo "Hello World!"
 echo "Welcome to Linux Shell Scripting Practice!"
-sleep 20
+sleep 10
 echo "Goodbye!"
 echo "See you next time!"
-sleep 15
+sleep 10
 echo "Exiting the script now."  
 echo "Script execution completed."
 echo "Have a great day!"
+echo "Thank you for practicing with me!"
+echo "Remember to keep practicing to improve your skills!"
+echo "This is the end of the script."
