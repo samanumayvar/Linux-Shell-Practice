2c4ed324eabb0ded273d0a44e8d908646a00ab31
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-26 15:36:33 +0530

    new one

--- Date.sh
+++ Date.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")  
-echo "Current date and time: $TIMESTAMP"
+echo "Current date and time: $TIMESTAMP"\
+START_TIME=$(date +%s)
+sleep 10
+END_TIME=$(date +%s)
+TOTAL_TIME=$((END_TIME - START_TIME))
+echo "Total time taken: $TOTAL_TIME seconds"
