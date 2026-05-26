d64455a41d81562d5bf07f9dc29d3b068ef6a77f
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-26 16:00:38 +0530

    new one

---
+++ specialvariables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "all variables passed to the script are: $@"
+echo "number of variables passed to the script are: $#" 
+echo "the name of the script is: $0"
+echo "the first variable passed to the script is: $1"
+echo "the second variable passed to the script is: $2"
+echo "the third variable passed to the script is: $3"
+echo "who is running the script: $USER"
