9bec6d311f7db4e6f33efb5ae81071120ca119b7
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-26 15:23:46 +0530

    new one

---
+++ confidential.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This is a confidential script. Please do not share it with anyone."
+echo "Please Enter Username:"
+read USER_NAME  
+echo "username is: $USER_NAME"
+echo "Please Enter Password:"
+read password
