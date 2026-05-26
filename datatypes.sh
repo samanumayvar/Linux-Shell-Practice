79b4afbdb84831ea62004f2ef37b5347de19b833
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-26 18:17:58 +0530

    new one

--- datatypes.sh
+++ datatypes.sh
@@ -6,7 +6,7 @@ echo "The sum of $NUM1 and $NUM2 is: $SUM"
 DIFF=$((NUM1 - NUM2))
 echo "The difference between $NUM1 and $NUM2 is: $DIFF"
 MOVIES ="Inception, The Matrix, Interstellar"
-echo "My favorite movies are: $MOVIES"
+echo "My favorite movies are:  ${MOVIES[0,1,2]}"
 echo "First movie is: ${MOVIES[0]}"
 echo "Second movie is: ${MOVIES[1]}"
 echo "Third movie is: ${MOVIES[2]}"
