93e17164a54c08f4c54a4e8e9829ae27f4d00e34
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-26 18:25:47 +0530

    new one

---
+++ conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NUMBER=$1
+if [ $NUMBER -gt 20 ]; then
+    echo "The number $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "The number $NUMBER is equal to 20"
+else
+    echo "The number $NUMBER is less than 20"
+fi
