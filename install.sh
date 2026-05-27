cb275a9e0b50a3d13c4b5ec15dbae12736ea073f
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-27 15:26:51 +0530

    newone

--- install.sh
+++ install.sh
@@ -3,5 +3,14 @@ USERID=$(id -u)
 echo "Current user ID: $USERID"
 if [ $USERID -ne 0 ]; then
     echo "This script must be run as root."
-    exit 0
+    exit 1
 fi
+echo "You are running as root user."
+
+echo "Installing MySQL server..."
+yum install -y mysql-server
+if [ $? -ne 0 ]; then
+    echo "Failed to install MySQL server."
+    exit 1
+fi
+echo "MySQL server installed successfully."
