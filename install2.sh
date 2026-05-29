825f743e0432ce0593c4a3396b5bc18162cfaa02
Sampath Bandarupelli <Sampath.Bandarupelli@ril.com>
2026-05-29 16:15:01 +0530

    good one

--- install2.sh
+++ install2.sh
@@ -4,14 +4,12 @@ if [ "$USERID" -ne 0 ]; then
     echo "ERROR: Please run this script with root access."
     exit 1
 fi
-if [$? -eq 0 ]; then
-    echo "You are running as root user."
-    yum install -y mariadb105-server
-    exit 0
-    
-    echo "MariaDB server installed successfully."
 
-else
-    echo "you are not running with root access."
+ yum install -y mariadb105-server
+
+if [$? -ne 0 ]; then
+    echo "failed to install MariaDB server."
     exit 1
+else
+    echo "MariaDB server installed successfully."
 fi
